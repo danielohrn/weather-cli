bd16eecf9fac5ea5b7951a9fd1f84001076c5c20
Daniel Öhrn <Daniel.ohrnhasslof@student.kyh.se>
2018-06-22 14:09:59 +0200

    add path module for resolving pathname to settings.json file

--- settings.js
+++ settings.js
@@ -1,8 +1,9 @@
 const fs = require('fs'); 
+const path = require('path'); 
 
 function readSettings() {
     return new Promise((resolve, reject) => {
-        fs.readFile('./settings.json', (err, settings) => {
+        fs.readFile(path.resolve(__dirname + '/settings.json'), (err, settings) => {
             if(err) return reject(err); 
             resolve(JSON.parse(settings.toString())); 
         }); 
@@ -11,7 +12,7 @@ function readSettings() {
 
 function writeSettings(settings) {
     return new Promise((resolve, reject) => {
-        fs.writeFile('./settings.json', settings, (err) => {
+        fs.writeFile(path.resolve(__dirname + '/settings.json'), settings, (err) => {
             if(err) return reject(err);
             resolve(settings); 
         }); 
